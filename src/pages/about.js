b352c0261642f309f4b2c07219b1917d78a1eede
Kaven Kim <kav@Kavens-Mac-mini.fios-router.home>
2022-02-03 15:34:47 -0500

    Finished Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/layout"
+
+const AboutPage = () => {
+    return (
+    <Layout pageTitle="About" pageHeading="About">
+        <main>
+        <p>They call me Kav, we'll figure out the rest later.</p>
+        </main>
+    </Layout>    
+
+    )
+}
+
+export default AboutPage
